bbc733d2ca30bf0e19f8679f3239916308d1ac91
Tyler Briles <t4b0@pge.com>
2017-05-30 13:14:58 -0700

    feat(aws): cli completetion
    https://github.com/aws/aws-cli/issues/1079

--- fish/config.fish
+++ fish/config.fish
@@ -3,4 +3,5 @@ fish_vi_key_bindings
 
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
-set -x PATH $PATH $HOME/.yarn/bin
+
+test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
